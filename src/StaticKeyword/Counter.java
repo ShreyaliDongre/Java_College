fa0d4556503509f52f2fef946e59944b3773556f
raylee <shreyali.d.10@gmail.com>
2026-06-13 14:33:13 +0530

    Static

--- src/StaticKeyword/Counter.java
+++ src/StaticKeyword/Counter.java
@@ -3,6 +3,9 @@ package StaticKeyword;
 public class Counter {
     static int count = 0;
     public Counter(){
-
+        count++;
+    }
+    public static void show(){
+        System.out.println("Counter is: " + count);
     }
 }
--- src/StaticKeyword/StaticFinal.java
+++ src/StaticKeyword/StaticFinal.java
@@ -1,6 +1,13 @@
 package StaticKeyword;
 
 public class StaticFinal {
-    Counter c1 = new Counter();
+    public static void main(String[] args) {
+//        Counter c1 = new Counter();
+        Counter.show();
+//        Counter c2 = new Counter();
+        Counter.show();
 
+
+
+    }
 }
